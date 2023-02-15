c966f398ca0878fc459dd4f0aedfaaaf93def292
shreikgristle <116908489+youngj3@users.noreply.github.com>
2023-02-15 14:49:17 -0500

    some style

--- client/src/components/layout/TopBar.js
+++ client/src/components/layout/TopBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SignOutButton from "../authentication/SignOutButton";
+import SearchBar from "./SearchBar.js";
 
 const TopBar = ({ user }) => {
   const unauthenticatedListItems = [
@@ -24,7 +25,7 @@ const TopBar = ({ user }) => {
     <div className="top-bar">
       <div className="top-bar-left">
         <ul className="menu">
-          <li className="menu-text">App</li>
+          <li className="menu-text">GigGuide</li>
           <li>
             <Link to="/">Home</Link>
           </li>
