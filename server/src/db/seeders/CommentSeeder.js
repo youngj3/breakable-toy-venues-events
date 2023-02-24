9c9e4d4b2c99604ad5171f0f63f99d51c01b337f
shreikgristle <116908489+youngj3@users.noreply.github.com>
2023-02-24 15:37:43 -0500

    comment seeder fix

--- server/src/db/seeders/CommentSeeder.js
+++ server/src/db/seeders/CommentSeeder.js
@@ -1,4 +1,5 @@
 import { Comment, Event, User } from '../../models/index.js'
+import { faker } from '@faker-js/faker'
 
 class CommentSeeder {
   static async seed(commentsPerPage){
@@ -6,16 +7,22 @@ class CommentSeeder {
     const users = await User.query()
     const commentsToSeed = []
 
-    if (commentsPerPage > users.length) {
-      commentsPerPage = users.length
-    }
-
     const sentences = [
       "Holy cow I cant believe they are coming to town!",
       "Went to a show back in '08, did NOT disappoint",
       "Been waiting for them to come to town!",
       "oh baby, lets go!",
-      "cannot express my joy over this!"
+      "cannot express my joy over this!",
+      "I'm over the moon that they're finally coming to town!",
+      "I've been counting down the days until this concert!",
+      "This is a dream come true!",
+      "I'm absolutely thrilled to see them perform live!",
+      "I'm beyond excited for this concert!",
+      "I'm on cloud nine knowing I get to see them in concert!",
+      "This is the concert of a lifetime and I can't wait!",
+      "My heart is racing with excitement for this concert!",
+      "I'm bursting with anticipation for this concert!",
+      "I'm so hyped for this concert, I can hardly contain myself!"
     ]
 
     for (const event of events) {
@@ -23,9 +30,8 @@ class CommentSeeder {
         const comment = {
           eventId: event.id,
           userId: users[i].id,
-          text: sentences[i]
+          text: faker.helpers.arrayElement(sentences)
         }
-        console.log(comment)
         commentsToSeed.push(comment)
       }
     }
