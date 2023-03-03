7fcbbc9b5741e644fd7483ea41f50df4737fb0f3
shreikgristle <116908489+youngj3@users.noreply.github.com>
2023-03-03 11:51:19 -0500

    top bar hides when different area is clicked

--- client/src/components/layout/TopBar.js
+++ client/src/components/layout/TopBar.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import SignOutButton from "../authentication/SignOutButton";
 import StateSelectionDropdown from "./state-select/StateSelectionDropdown";
 
 const TopBar = ({ user }) => {
   const [showMenu, setShowMenu] = useState(false)
+  const dropdownRef = useRef(null);
 
   const unauthenticatedListItems = [
     <li key="sign-in">
@@ -30,6 +31,20 @@ const TopBar = ({ user }) => {
     setShowMenu(showMenu ? false : true)
   }
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    window.addEventListener("click", handleClickOutside);
+
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, [dropdownRef]);
+
   return (
     <div className="top-bar">
       <div className="top-bar-left">
@@ -38,8 +53,8 @@ const TopBar = ({ user }) => {
           <li>
             <Link to="/">Home</Link>
           </li>
-          <div className="dropdown">
-            <input type="button" className="dropbtn" onClick={showStateMenu} value="Our Locations" />
+          <div className="dropdown" ref={dropdownRef}>
+            <input type="button" className="dropbtn" onClick={showStateMenu} value="Our Locations"/>
               <StateSelectionDropdown showMenu={showMenu}/>
           </div>
         </ul>
